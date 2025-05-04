aa91b18d27dabe24856621ab97763d7377098b7f
Amila Sampath <amilasampath0130@gmail.com>
2025-05-04 14:35:43 +0530

    basic Ui components
    this include the basic Ui design in our dashboard

---
+++ wcsc_leaderboard/app/(pages)/leaderboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LEADERBOARD = () => {
+  return (
+    <div>LEADERBOARD</div>
+  )
+}
+
+export default LEADERBOARD
---
+++ wcsc_leaderboard/app/_config/nav_config.ts
@@ -0,0 +1,16 @@
+
+export type NavTab = {
+    name: string;
+    path: string;
+  };
+  
+  export const navTabs: NavTab[] = [
+    { name: "Dashboard", path: "/" },
+    { name: "LeaderBoard", path: "/leaderboard" },
+    { name: "Practice", path: "/Practice" },
+    { name: "Compettion", path: "/compettion" },
+    { name: "Contents", path: "/contents" },
+    { name: "About", path: "/about" },
+    { name: "Profile", path: "/profile" },
+  ];
+  
